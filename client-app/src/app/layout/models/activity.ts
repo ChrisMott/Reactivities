9f13f9dae17a1ac56b9c34665a95ed875e08d700
Chris Mott <9664841+ChrisMott@users.noreply.github.com>
2020-07-02 00:17:07 -0500

    End of Section 5. CRUD in ReactJS

---
+++ client-app/src/app/layout/models/activity.ts
@@ -0,0 +1,11 @@
+import { StrictIconGroupProps } from "semantic-ui-react";
+
+export interface IActivity{
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    date: string;
+    city: string;
+    venue: string;
+}
